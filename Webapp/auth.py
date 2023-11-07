f04c64dabaff47a6b9f73fb981a20a6927d91029
Chryxcx <ianpetilla@gmail.com>
2023-11-07 08:14:58 +0800

    updated

--- Webapp/auth.py
+++ Webapp/auth.py
@@ -7,8 +7,11 @@ from pyzbar.pyzbar import decode
 import tkinter as tk
 from tkinter import Label
 from PIL import Image, ImageTk
+import mysql.connector
 from io import BytesIO
+import time
 import base64
+import requests
 import threading
 
 
@@ -51,15 +54,17 @@ def inventory():
             return render_template('inventory.html')
 
         cursor = db.cursor()
+
+        qr_code_data_list = get_qr_data(cursor)
         
         if category == 'all':
             # Fetch all rows from the selected shelf
-            query = f"SELECT id, category, title, publisher, year FROM {shelf}"
+            query = f"SELECT id, category, isbn, title, publisher, year_published FROM {shelf}"
             print("Query:", query)
             cursor.execute(query)
         else:
             # Fetch rows from the selected shelf with the specified category
-            query = f"SELECT id, category, title, publisher, year FROM {shelf} WHERE category = %s"
+            query = f"SELECT id, category, isbn, title, publisher, year_published FROM {shelf} WHERE category = %s"
             print("Query:", query)
             cursor.execute(query, (category,))
         
@@ -107,7 +112,16 @@ def inventory():
         print("Exception:", e)
         return jsonify({'error': 'An error occurred during data retrieval'})
        
-       
+def get_qr_data(cursor):
+    try:
+        cursor.execute("SELECT * FROM shelf1")
+        result = cursor.fetchall()
+        qr_values = [row[1] for row in result]  # assuming the QR data is in the second column
+        return qr_values  # You can return the list directly unless you specifically need JSON format here
+    except mysql.connector.Error as err:
+        print(f"SQL Error: {err}")
+        return []  # Return an empty list in case of an error
+
 
 @auth.route('/qr_codes/<image_id>')
 def serve_qr_code(image_id):
@@ -130,6 +144,19 @@ camera = cv2.VideoCapture(0)  # Initialize the camera when the server starts.
 def camera_view():
     return render_template('camera.html')
 
+def retrieve_data():
+    try:
+        response = requests.get("/inventory")
+        if response.status_code == 200:
+            qr_values = response.json()
+            return set(qr_values)
+        else:
+            print("Error fetching data from API:", response.text)
+            return set()
+    except Exception as e:
+        print("Error fetching data from API:", str(e))
+        return set()
+
 def generate_frames():
     global streaming, camera
 
@@ -143,11 +170,18 @@ def generate_frames():
         if not ret:
             print("Error: Frame could not be read.")
             break
+        
+        qr_values_from_api = retrieve_data()
 
         decoded_objects = decode(frame)
         for obj in decoded_objects:
             x, y, w, h = obj.rect
             qr_data = obj.data.decode('utf-8')
+
+            if qr_data in qr_values_from_api:
+                color = (0, 255, 0)  # Green for QR codes that are in inventory
+            else:
+                color = (0, 0, 255)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             cv2.putText(frame, qr_data, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
 
@@ -169,6 +203,12 @@ def start_stream():
 def stop_stream():
     global streaming
     streaming = False
+
+    time.sleep(0.1)
+
+    if camera.isOpened():
+        camera.release()
+    
     return "Streaming stopped"
 
 @auth.route('/video_feed')
@@ -179,133 +219,52 @@ def video_feed():
 def insert():
     db = current_app.get_db()
     cursor  = db.cursor()
-    cursor1 = db.cursor()
     cursor.execute('SELECT * FROM categories')
     category_name = cursor.fetchall()
     print(category_name)
-    cursor1.execute('SELECT * FROM shelves')
-    shelf = cursor1.fetchall()
+    cursor.execute('SELECT * FROM shelves')
+    shelf = cursor.fetchall()
 
 
     if request.method == "POST":
         db = current_app.get_db()
+        print(db)
         cursor = db.cursor()
         table = request.form.get('b_shelves')
-        cate = request.form.get('b_cata')
+        print(table)
+        cate = request.form.get('b_cate')
+        print(cate)
+        isbn = request.form.get('b_isbn')
         title = request.form.get('b_title')
         publisher = request.form.get('b_publisher')
         year = int(request.form.get('b_year'))
 
         try:
-            table_prefixes = {
-                'shelf1': {'prefix': 'A-', 'counter': 'shelf1_counter'},
-                'shelf2': {'prefix': 'B-', 'counter': 'shelf2_counter'},
-                'shelf3': {'prefix': 'C-', 'counter': 'shelf3_counter'},
-                'shelf4': {'prefix': 'D-', 'counter': 'shelf4_counter'},
-                # Add more tables and prefixes as needed
-            }
-
-            if table not in table_prefixes:
-                return jsonify({'success': False, 'error': 'Table not found'})
-
-            table_info = table_prefixes[table]  # Get table-specific info
-
-            # Define initial counter values for categories
-            category_to_initial_counter = {
-                'Science': 99,
-                'Language': 199,
-                'Novel': 299,
-                'Math': 0
-            }
             
-            max_counters = {
-                ('Science', 'shelf1'): 199,
-                ('Language', 'shelf1'): 299,
-                ('Novel', 'shelf1'): 399,
-                ('Math', 'shelf1'): 99,
-                ('Science', 'shelf2'): 199,
-                ('Language', 'shelf2'): 299,
-                ('Novel', 'shelf2'): 399,
-                ('Math', 'shelf2'): 99,
-                ('Science', 'shelf3'): 199,
-                ('Language', 'shelf3'): 299,
-                ('Novel', 'shelf3'): 399,
-                ('Math', 'shelf3'): 99,
-                ('Science', 'shelf4'): 199,
-                ('Language', 'shelf4'): 299,
-                ('Novel', 'shelf4'): 399,
-                ('Math', 'shelf4'): 99,
-                # Add more category and shelf combinations as needed
-            }
-            
-            if (cate, table) not in max_counters:
-                return jsonify({'success': False, 'error': 'Invalid category or shelf'})
+            query = f"INSERT INTO {table} (category, isbn, title, publisher, year_published) VALUES (%s, %s, %s, %s, %s)"
 
-            max_counter_value = max_counters[(cate, table)]
-
-            # Check if the selected category is in the mapping
-            if cate not in category_to_initial_counter:
-                return jsonify({'success': False, 'error': 'Invalid category'})
-
-            # Retrieve the current counter value for the selected category
-            cursor.execute(f"SELECT {table_info['counter']} FROM counters WHERE category = %s", (cate,))
-            counter_result = cursor.fetchone()
-
-            if counter_result is not None:
-                current_counter = counter_result[0]
-            else:
-                current_counter = category_to_initial_counter[cate]
-
-            # Calculate the next counter value
-            next_counter_value = current_counter + 1
-            
-            if next_counter_value > max_counter_value:
-                return jsonify({'success': False, 'error': 'Category and shelf counter limit reached'})
-
-            # Reset the counter for the previous category to its current value
-            if 'current_category' in session:
-                prev_category = session['current_category']
-                cursor.execute(f"SELECT {table_info['counter']} FROM counters WHERE category = %s", (prev_category,))
-                prev_counter_result = cursor.fetchone()
-                if prev_counter_result is not None:
-                    prev_counter = prev_counter_result[0]
-                    cursor.execute(f"UPDATE counters SET {table_info['counter']} = %s WHERE category = %s", (prev_counter, prev_category))
-                    db.commit()
-
-
-            # Update the counter with the new value for the selected category
-            cursor.execute(f"UPDATE counters SET {table_info['counter']} = %s WHERE category = %s", (next_counter_value, cate))
+            cursor.execute(query, (cate, isbn, title, publisher, year))
             db.commit()
 
-            # Construct the book ID by combining the prefix and the counter value
-            id = f'{table_info["prefix"]}{next_counter_value}'
-            
-            # Update the current category in the session
-            session['current_category'] = cate
-            
-
-            query = f"INSERT INTO {table} (id, category, title, publisher, year) VALUES (%s,%s, %s, %s, %s)"
-
-            cursor.execute(query, (id, cate, title, publisher, year))
-            db.commit()
+            id = cursor.lastrowid
 
             qr_data = f"ID: {id}, Title: {title}"
             qr_data_encoded = qr_data.encode('utf-8')
             qr_image_base64, image_path = current_app.generate_qr_code(qr_data_encoded, str(id))
-
-            cursor.close()
-            db.close()
 
             flash('Data successfully inserted!', 'success')
 
             return jsonify({'success': True, 'qr_image_base64': qr_image_base64, 'image_path': image_path, 'qrCodeID': id})
 
         except Exception as e:
+            db.rollback()
             print("Exception:", e)
-            cursor.close()
-            db.close()
 
             flash('Error inserting data into the database', 'error')
-            return jsonify({'success': False, 'error': 'Error inserting data into the database'})
+            return jsonify({'success': False, 'error': str(e)})
+
+        finally:
+            cursor.close()
+            db.close()
 
     return render_template("insert.html", qr_image_base64=None, category_name=category_name, shelf=shelf)
